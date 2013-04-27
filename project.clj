d7a336e08f20af3d1ffe884f8dffe0599c3a7025
Aysylu <aysylu7@gmail.com>
2013-04-27 18:13:42 -0400

    added dependency on titanium

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojurewerkz/titanium "1.0.0-alpha2"]]
   :profiles {:dev 
              {:dependencies [[org.clojure/clojure "1.5.1"]]}})
