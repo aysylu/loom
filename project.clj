71cc967c304e0850bf0d57cdefaa5269b51a454f
Aysylu <aysylu7@gmail.com>
2013-04-27 20:39:43 -0400

    updated titanium version to alpha4

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojurewerkz/titanium "1.0.0-alpha2"]]
+                 [clojurewerkz/titanium "1.0.0-alpha4-SNAPSHOT"]]
   :profiles {:dev 
              {:dependencies [[org.clojure/clojure "1.5.1"]]}})
