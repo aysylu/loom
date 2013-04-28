ba74d8bb602cd504887894e88118da8efd880c67
Aysylu <aysylu7@gmail.com>
2013-04-27 20:42:45 -0400

    implemented graph view for titanium with graphviz; updated to new functions from alpha4

--- src/loom/titanium.clj
+++ src/loom/titanium.clj
@@ -5,6 +5,7 @@
   (:require [clojurewerkz.titanium.elements :as te])
   (:require [clojurewerkz.titanium.edges :as edges])
   (:require [clojurewerkz.titanium.vertices :as nodes])
+  (:require loom.io)
   (:use [loom.graph]))
 
 (defn titanium->loom
@@ -13,8 +14,8 @@
   (reify
     Graph
     ; implement graph methods
-    (nodes [_] (tg/get-vertices titanium-graph))
-    (edges [_] (map (juxt edges/head-vertex edges/tail-vertex) (tg/get-edges titanium-graph)))
+    (nodes [_] (nodes/get-all-vertices))
+    (edges [_] (map (juxt edges/head-vertex edges/tail-vertex) (edges/get-all-edges)))
     (has-node? [g node] (some #(= % node) (nodes g)))
     (has-edge? [g n1 n2] (some #(= % [n1 n2]) (edges g)))
     (neighbors [g] (partial neighbors g))
@@ -27,17 +28,27 @@
     (in-degree [g node] (count (neighbors g node)))
     ))
 
-(def dg (digraph [1 2] [2 3] {3 [4 5] 5 [6 7]} 7 8 9))
-(degree dg 5)
-(require 'loom.io)
-(let [g (tg/open-in-memory-graph)
-            v1 (tg/add-vertex g {:age 28 :name "Michael"})
-            v2 (tg/add-vertex g {:age 26 :name "Alex"})
-            e  (tg/add-edge g v1 v2 "friend" {:since 2008})
-            xs (set (tg/get-edges g))]
-      (loom.io/view (titanium->loom g)))
-(let [g  (tg/open-in-memory-graph)
-            v1 (tg/add-vertex g {:age 28 :name "Michael"})
-            v2 (tg/add-vertex g {:age 26 :name "Alex"})
-            xs (set (tg/get-vertices g))]
-      (println xs))
+(defn view [g]
+  (loom.io/view (if (satisfies? Graph g)
+                  g
+                  (titanium->loom g))
+                :node-label nodes/to-map
+                :edge-label
+                (fn [n1 n2]
+                  (println n1 n2)
+                  (println (edges/edges-between n1 n2))
+                  (println (edges/edges-between n2 n1))
+                  (mapv edges/to-map
+                    (edges/edges-between n2 n1)))))
+
+(let [g (tg/open {"storage.backend" "inmemory"})]
+  (tg/transact!
+    (let [ 
+          v1 (nodes/create! {:age 28 :name "Michael"})
+          v2 (nodes/create! {:age 26 :name "Alex"})
+          e (edges/connect!
+              v1
+              "friend"
+              v2
+              {:since 2008})]
+      (view (titanium->loom g)))))
