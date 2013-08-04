220fe9d98b589bdf2a0d3dda1e109ce938b96cea
Aysylu <aysylu7@gmail.com>
2013-08-03 20:35:46 -0400

    Initial work on ssa->loom integration + dataflow analysis framework

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojurewerkz/titanium "1.0.0-beta1"]]
+                 [io.rkn/core.async "0.1.0-SNAPSHOT"]] 
   :profiles {:dev 
              {:dependencies [[org.clojure/clojure "1.5.1"]]}})
