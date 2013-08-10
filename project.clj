c85d09b1182aff13f593567e7d355113d1052532
Aysylu <aysylu7@gmail.com>
2013-08-10 16:46:49 -0400

    updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aysylu/loom "0.3.1"
+(defproject aysylu/loom "0.3.2-SNAPSHOT"
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
