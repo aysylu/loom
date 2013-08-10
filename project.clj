280414e33248b694c779c973ee841d289962c5f3
Aysylu <aysylu7@gmail.com>
2013-08-10 16:41:30 -0400

    updated version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject heffalump/loom "0.3.0-SNAPSHOT"
+(defproject aysylu/loom "0.3.1"
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
+  :url "https://github.com/aysylu/loom"
   :profiles {:dev 
              {:dependencies [[org.clojure/clojure "1.5.1"]]}})
