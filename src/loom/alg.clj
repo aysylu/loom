575748e1db03e23a82fb9879168ae408b696e36e
Reid McKenzie <rmckenzie92@gmail.com>
2013-11-03 12:55:03 -0600

    Improvement: Use clojure.core.memoize
    Pull in the clojure.core.memoize library in the interests of efficient
    memory usage. This may come at a performance hit, but will ensure that
    structural-hash can't suck up and sit on huge amounts of memory until
    the JVM dies.

--- src/loom/alg.clj
+++ src/loom/alg.clj
@@ -4,7 +4,8 @@ can use these functions."
       :author "Justin Kramer"}
   loom.alg
   (:require [loom.alg-generic :as gen]
-            [loom.flow :as flow])
+            [loom.flow :as flow]
+            clojure.core.memoize)
   (:use [loom.graph
          :only [add-edges nodes edges successors weight predecessors out-degree
                 in-degree weighted? directed? graph transpose]
@@ -418,39 +419,6 @@ can use these functions."
                                                  method-set))))]
     [flow-map flow-value]))
                                                    
-                                                   
-(defn- assoc-noclobber 
-  "An assoc wrapper which ensures that existing keys will not be
-  clobbered by subsequent assoc invocations. 
-
-  Used as a helper for locking-memoize to ensure that (delay) refs
-  cannot be lost by swap! retry behavior."
-  
-  [m k v]
-  (if (contains? m k) m
-      (assoc m k v)))
-
-(defn- locking-memoize 
-  "Memoizes the function f, using the same approach as
-  clojure.core/memoize. The practical difference is that this function
-  provides the gurantee that in spite of parallel invocations of the
-  memoized function each input to f will only ever be memoized
-  once. This resolves an implementation detail in clojure.core/memoize
-  which allows f to be applied to args without locking the cache to
-  prevent other threads duplicating the work."
-
-  [f]
-  (let [mem (atom {})]
-    (fn [ & args ]
-      (if (= :_SKETCHY_TESTING_RESET
-             (first args))
-        (reset! mem {})
-        (if-let [e (find @mem args)]
-          (deref (val e))
-          (-> (swap! mem assoc-noclobber
-                     args (delay (apply f args)))
-              (get args)
-              (deref)))))))
 
 (def ^:dynamic *hash-algo* "MD5")
 
@@ -467,7 +435,7 @@ can use these functions."
           (apply str))))
 
 (def -node-hash
-  (locking-memoize
+  (clojure.core.memoize/lu
    (fn [graph seen node]
      (let [children (-> graph
                         (nb node)
@@ -485,7 +453,8 @@ can use these functions."
                   (pmap (partial -node-hash graph (conj seen node)))
                   (sort)
                   (apply str)
-                  (-hash)))))))
+                  (-hash)))))
+   :lu/threshold 256))
 
 (defn structural-hash
   "Returns an hash string, representing the structure of the input as
