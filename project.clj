5a91ee461e9e73101b1be4c3027893338528c489
Aysylu <aysylu7@gmail.com>
2014-06-08 22:41:52 -0400

    moved to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aysylu/loom "0.5.0"
+(defproject aysylu/loom "0.5.1-SNAPSHOT"
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
