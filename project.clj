3e0628d890a4203e1bed96fd3d944df7b62a1de7
Daniel Compton <desk@danielcompton.net>
2015-05-06 08:47:14 +1200

    Upgrade to Clojure 1.6

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "Graph library for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.priority-map "0.0.5"]]
   :url "https://github.com/aysylu/loom"
   :test-selectors {:default (fn [m] (not (:test-check-slow m)))
                    :all (constantly true)
                    :test-check-slow :test-check-slow}
   :profiles {:dev
-             {:dependencies [[org.clojure/clojure "1.5.1"]
+             {:dependencies [[org.clojure/clojure "1.6.0"]
                              [org.clojure/test.check "0.5.7"]]}}
   :aliases {"release" ["do" "clean," "with-profile" "default" "deploy" "clojars"]}
 
