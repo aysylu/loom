7a1b51f7cc8879509105e0a932600ba871676028
Aysylu <aysylu@users.noreply.github.com>
2015-07-11 19:23:20 -0400

    moved dataflow analysis framework from the ssa branch

---
+++ src/loom/dataflow.clj
@@ -0,0 +1,31 @@
+(ns ^{:doc "Dataflow analysis framework"
+      :author "Aysylu Greenberg"}
+  loom.dataflow
+  (:require [loom.graph :as g]))
+
+(defn dataflow-analysis
+  "Performs dataflow analysis using iterative worklist-based algorithm.
+   Must provide the graph and its start node, join and transfer functions."
+  [& {:keys [start graph join transfer]}]
+  (let [start (cond
+                (set? start) start
+                (coll? start) (set start)
+                :else #{start})]
+    (loop [out-values {} 
+           queue (into clojure.lang.PersistentQueue/EMPTY start)]
+      (let [node (peek queue)
+            worklist (pop queue)
+            in-value (join (mapv out-values (g/predecessors graph node)))
+            out (transfer node in-value)
+            update? (not= out (get out-values node))
+            out-values (if update?
+                         (assoc out-values node out)
+                         out-values)
+            workset (set worklist)
+            worklist (if update?
+                       (->> (g/successors graph node)
+                            (remove workset)
+                            (into worklist)))]
+        (if (seq worklist)
+          (recur out-values worklist)
+          out-values)))))
