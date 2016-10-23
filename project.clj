41a6dbe33bd64030e94d2a3037554d9ebe81268b
Chas Emerick <chas@cemerick.com>
2016-10-23 07:29:25 -0400

    project configuration tweaking to make ClojureScript testing easy (interactively and in CI)

--- project.clj
+++ project.clj
@@ -5,29 +5,40 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.priority-map "0.0.5"]
-                 [tailrecursion/cljs-priority-map "1.1.0"]]
+                 [tailrecursion/cljs-priority-map "1.2.0"]]
   :url "https://github.com/aysylu/loom"
   :test-selectors {:default (fn [m] (not (:test-check-slow m)))
                    :all (constantly true)
                    :test-check-slow :test-check-slow}
   
   :profiles {:dev [:cljs
-                   {:dependencies [[lein-doo "0.1.7"]
-                                   [org.clojure/test.check "0.9.0"]
-                                   [com.cemerick/piggieback "0.2.1"]]
-                    :repl-options {:init (set! *print-length* 50)
-                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}]
+                   {:dependencies [[org.clojure/test.check "0.9.0"]]
+                    :plugins [[com.jakemccrary/lein-test-refresh "0.15.0"]]
+                    :repl-options {:init (set! *print-length* 50)}}]
 
              :cljs {:dependencies [[org.clojure/clojurescript "1.9.89"]]
-                    :plugins [[lein-cljsbuild "1.1.3" :exclusions [org.clojure/clojure]]]
+                    :plugins [[lein-cljsbuild "1.1.3" :exclusions [org.clojure/clojure]]
+                              [lein-doo "0.1.7"]]
+                    :doo {:build "node-dev"}
                     :cljsbuild {:builds
-                                [{:id "min"
-                                  :source-paths ["src"]
-                                  :compiler {:output-to "target/classes/public/js/viewer.js"
+                                {"node-dev"
+                                 {:source-paths ["src", "test"]
+                                  :compiler {:output-to "target/loom.js"
+                                             :optimizations :none
+                                             :pretty-print true
+                                             :target :nodejs
+                                             :main loom.test.runner}}
+                                 "node-test"
+                                 {:id "min"
+                                  :source-paths ["src", "test"]
+                                  :compiler {:output-to "target/loom.js"
                                              :optimizations :advanced
-                                             :pretty-print false}}]}}} 
+                                             :pretty-print false
+                                             :target :nodejs
+                                             :main loom.test.runner}}}}}}
   
-  :aliases {"test" ["do" "clean," "test" ":all"]
+  :aliases {"test-all" ["do" "clean," "test" ":all," "cljs-test"]
+            "cljs-test" ["doo" "node" "node-test" "once"]
             "release" ["do" "clean," "with-profile" "default" "deploy" "clojars"]}
 
   :plugins  [[codox "0.8.12"]]
