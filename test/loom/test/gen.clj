fb926669214fe8478cbe795a3211ea30d57ff375
hpcjkour <hpcjkour@hlrs.de>
2018-09-12 17:33:09 +0200

    Remove gen-barabasi-albert and gen-barabasi-albert-test

--- test/loom/test/gen.clj
+++ test/loom/test/gen.clj
@@ -67,30 +67,3 @@
       (is (and
             (<= (- (/ 3 4) 0.2) (clustering-coefficient g2))
             (>= (+ (/ 3 4) 0.2) (clustering-coefficient g2)))))))
-
-(deftest gen-barabasi-albert-test
-  (let [g (graph)
-        percentage (fn [num percent]
-                     (* (/ percent 100) num))
-        expected-edge-count (fn [num-nodes]
-                              (+ 2 (* 2 (dec num-nodes))))]
-    (testing "Construction"
-      (are [graphs] loom.graph/graph?
-        (gen-barabasi-albert g 10 1)
-        (gen-barabasi-albert g 20 2)
-        (gen-barabasi-albert g 42 5)))
-    (testing "Node Count"
-      ;; Because creating the graph involves probabilistic decisions
-      ;; the actual number of nodes may be a bit lower than the expected count
-      (are [num-nodes degree] (< (percentage num-nodes 95) (count (nodes (gen-barabasi-albert g num-nodes degree))))
-        200 3
-        100 1
-        567 7
-        980 20))
-    (testing "Edge Count"
-      ;; same problem as with node count
-      (are [num-nodes degree] (< (percentage (expected-edge-count num-nodes) 95) (count (edges (gen-barabasi-albert g num-nodes degree))))
-        200 3
-        100 1
-        567 7
-        980 20))))
